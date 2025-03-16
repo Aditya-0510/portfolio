71b03b19fe43b24bec27df8b10046c952021329b
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2025-03-16 19:36:48 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
